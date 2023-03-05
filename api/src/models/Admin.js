b41a8d5e3406bdaed6ebc6e6394cb692fcb04050
Samuel <samuel.esba@hotmail.com>
2023-03-04 21:36:18 -0400

    Modelo Admin

---
+++ api/src/models/Admin.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Admin",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
