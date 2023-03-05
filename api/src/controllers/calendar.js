59bcbe55047160841f64e923d01b4794889c8bf0
Deybi1004 <deybiorrego@gmail.com>
2023-03-04 23:07:11 -0500

    Horarios en Calendario

--- api/src/controllers/calendar.js
+++ api/src/controllers/calendar.js
@@ -1,6 +1,27 @@
 const {Calendar} = require ('../db');
 
 const createCalendar = async() => {
+    //Create Schedule 
+        let horaInicio = "9:00"
+        let horaFinal = "21:00"
+        const franjaHoraria = [];
+        const horaInicioParts = horaInicio.split(':');
+        const horaFinalParts = horaFinal.split(':');
+        let hora = parseInt(horaInicioParts[0]);
+        let minutos = parseInt(horaInicioParts[1]);
+      
+        while (hora < parseInt(horaFinalParts[0]) || (hora === parseInt(horaFinalParts[0]) && minutos <= parseInt(horaFinalParts[1]))) {
+          const horarioFraccionado = {
+            hora: `${hora.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`,
+            reserved: false
+          };
+          franjaHoraria.push(horarioFraccionado);
+          minutos += 15;
+          if (minutos === 60) {
+            minutos = 0;
+            hora += 1;
+          }
+        }
     //Create calendar (30 days from now)
     const dates = [];
     const actuallyDate = new Date();
@@ -8,7 +29,8 @@ const createCalendar = async() => {
       const date = new Date(actuallyDate.getFullYear(), actuallyDate.getMonth(), actuallyDate.getDate() + i);
       const day = {
         date,
-        full: false
+        full: false,
+        schedule:franjaHoraria
       };
       dates.push(day);
     }
