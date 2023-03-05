d84f2d58a36b6777beb248f28a23945fa75c749c
Samuel <samuel.esba@hotmail.com>
2023-03-04 23:51:42 -0400

    feature/controllers&routes Controlador y rutas para el modelo Admin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,19 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const allBarbers = require("./allBarbers.js");
 const router = Router();
+const allBarbers = require("./allBarbers.js");
 const calendarRoute= require ('./calendarRoutes.js')
+const adminRoutes = require('./adminRoutes.js');
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use("/barbers", allBarbers);
 
-router.use('/calendar', calendarRoute)
+router.use('/calendar', calendarRoute);
+
+router.use('/admins', adminRoutes);
 
 
 module.exports = router;
