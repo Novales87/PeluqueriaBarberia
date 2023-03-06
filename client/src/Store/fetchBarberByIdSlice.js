6b7ffe4c138df55917c72cc26ea08c8eac80fb9e
Your Name <you@example.com>
2023-03-05 21:31:50 -0300

    hice el componente que modifica el barbero, el que los muestra en lista, y los slice de redux

---
+++ client/src/Store/fetchBarberByIdSlice.js
@@ -0,0 +1,81 @@
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// // Creamos un action asíncrono utilizando createAsyncThunk
+// export const fetchBarberById = createAsyncThunk(
+//   "barberById/fetchBarberById",
+//   async (id) => {
+//     const response = await axios.get(`http://localhost:3001/api/barbers/${id}`);
+//     return response.data.data;
+//   }
+// );
+
+// // Creamos un slice con la información de un barbero por ID
+// const barberByIdSlice = createSlice({
+//   name: "barberById",
+//   initialState: {
+//     barber: null,
+//     status: "idle",
+//     error: null,
+//   },
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchBarberById.pending, (state) => {
+//         state.status = "loading";
+//       })
+//       .addCase(fetchBarberById.fulfilled, (state, action) => {
+//         state.status = "succeeded";
+//         state.barber = action.payload;
+//       })
+//       .addCase(fetchBarberById.rejected, (state, action) => {
+//         state.status = "failed";
+//         state.error = action.error.message;
+//       });
+//   },
+// });
+
+// export default barberByIdSlice.reducer;
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchBarberById = createAsyncThunk(
+  "barberById/fetchBarberById",
+  async (id) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/api/barbers/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      throw Error(error.response.data.message);
+    }
+  }
+);
+
+const barberByIdSlice = createSlice({
+  name: "barberById",
+  initialState: {
+    barber: null,
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBarberById.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchBarberById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.barber = action.payload;
+      })
+      .addCase(fetchBarberById.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default barberByIdSlice.reducer;
