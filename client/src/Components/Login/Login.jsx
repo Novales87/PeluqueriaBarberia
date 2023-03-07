a510bff26644ce53c8d60ddfd66ea9fd70e8bc77
Deybi1004 <deybiorrego@gmail.com>
2023-03-06 20:54:21 -0500

    divs

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -6,43 +6,49 @@ function Login() {
 
   //Verificación de Gmail
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const gmailPattern = /^[a-zA-Z0-9._%+-]+@gmail.com$/i;
     console.log(email, password);
 
-    if(email.match(gmailPattern)) {
-        setPassword("");
-        setEmail("");
-
+    if (email.match(gmailPattern)) {
+      setPassword("");
+      setEmail("");
     } else {
-        alert("Por favor ingrese una dirección de Gmail válida");
+      alert("Por favor ingrese una dirección de Gmail válida");
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="email"> Correo Electrónico: </label>
-      <input
-        type="text"
-        id="email"
-        value={email}
-        onChange={(e) =>setEmail(e.target.value)}
-        autoComplete="off"
-        required
-      />
-      <label htmlFor="password"> Contraseña: </label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        autoComplete="off"
-        required
-      />
-
-      <button type="submit">Click Me</button>
+      <div>
+        <label htmlFor="email"> Correo Electrónico: </label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoComplete="off"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password"> Contraseña: </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="off"
+          required
+        />
+      </div>
+      
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <button className="btn btn-primary" type="submit">
+          Sign In
+        </button>
+      </div>
     </form>
   );
 }
