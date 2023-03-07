7c33628782226d4dfb28be0fcdc04931245e2adf
ZTPspartan <alfonsos99710@gmail.com>
2023-03-07 16:25:05 -0500

    Login structura

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.scss";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -21,51 +22,55 @@ function Login() {
   };
 
   return (
-    <form
-      style={{ display: "flex", flexDirection: "column", alignItems: "Center" }}
-      onSubmit={handleSubmit}
-    >
-      <div>
-        <h1>Please sig in</h1>
-      </div>
-      <div>
+    <div className="Login-Flex">
+      <form
+        onSubmit={handleSubmit}
+        className="Login-Container"
+      >
         <div>
-
-        <label htmlFor="email"> Correo Electrónico: </label>
+          <h1>Please sig in</h1>
         </div>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="off"
-          required
-        />
-      </div>
-
-      <div>
         <div>
+          {/* <div>
+            <label htmlFor="email"> Correo Electrónico: </label>
+          </div> */}
+          <input
+            placeholder="Correo Electrónico:"
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        <div>
+          {/* <div>
           <label htmlFor="password"> Contraseña: </label>
+        </div> */}
+          <input
+            placeholder="Contraseña"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="off"
+            required
+          />
         </div>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="off"
-          required
-        />
-      </div>
 
-      <div className="d-grid gap-2 col-6 mx-auto">
-        <button className="btn btn-primary" type="submit">
-          Sign In
-        </button>
-      </div>
-      <div>
-        <h4>© 2022-2023</h4>
-      </div>
-    </form>
+        <div className="d-grid gap-2 col-6 mx-auto">
+          <button className="btn btn-primary" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div>
+          <h4>© 2022-2023</h4>
+        </div>
+      </form>
+    </div>
+
   );
 }
 
