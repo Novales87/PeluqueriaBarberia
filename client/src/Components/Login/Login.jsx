56eae08938eaa3134125107c51b9ee23f97b72ab
Deybi1004 <deybiorrego@gmail.com>
2023-03-07 21:01:44 -0500

    animation Login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -23,23 +23,16 @@ function Login() {
   };
 
   return (
-    <div className="Login-container">
-      <img className="logo-barber" src ={logo} alt ="The Cutman"/>
-      <form
-        onSubmit={handleSubmit}
-        className="Login-form-container"
-      >
-        
-          <h1 className="title-sign-in">Sign in</h1>
-        
-        
-         <div className="container-gmail">
+    <div className="Login-container "> 
+      <img className="logo-barber fade-in-logo" src ={logo} alt ="The Cutman"/>
+      <form onSubmit={handleSubmit} className="Login-form-container fade-in">
+        <h1 className="title-sign-in">Sign in</h1>
+        <div className="container-gmail">
           <div className="gmail-box">
-
-         <i class='bx bx-envelope' style={{color:"#ffffff" }} ></i>
+            <i class='bx bx-envelope' style={{color:"#ffffff" }} ></i>
           </div>
           <input
-          className="input-gmail"
+            className="input-gmail"
             placeholder="Correo Electrónico"
             type="text"
             id="email"
@@ -48,20 +41,13 @@ function Login() {
             autoComplete="off"
             required
           />
-         </div>
-        
-
-        
-          {/* <div>
-          <label htmlFor="password"> Contraseña: </label>
-        </div> */}
-       
-         <div className="container-password">
-            <div className="password-box">
+        </div>
+        <div className="container-password">
+          <div className="password-box">
             <i class='bx bx-lock' style={{color:"#ffffff" }} ></i>
-            </div>
+          </div>
           <input
-          className="input-password"
+            className="input-password"
             placeholder="Contraseña"
             type="password"
             id="password"
@@ -70,22 +56,15 @@ function Login() {
             autoComplete="off"
             required
           />
-         </div>
-          
-        
-        
-
+        </div>
         <div className="d-grid gap-2 col-6 mx-auto">
           <button className="btn btn-primary" type="submit">
             Sign In
           </button>
         </div>
-        
-          <h4 className="reserved-c">© 2022-2023</h4>
-        
+        <h4 className="reserved-c">© 2022-2023</h4>
       </form>
     </div>
-
   );
 }
 
