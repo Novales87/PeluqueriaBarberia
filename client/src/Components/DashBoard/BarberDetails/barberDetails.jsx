7da21ead1e754aba6c48a6bccd41b8249d683153
Your Name <you@example.com>
2023-03-10 18:46:14 -0300

    solucione problema logico en barberDetails

--- client/src/Components/DashBoard/BarberDetails/barberDetails.jsx
+++ client/src/Components/DashBoard/BarberDetails/barberDetails.jsx
@@ -1,41 +1,3 @@
-// import { useEffect, useState } from "react";
-// import "./BarberDetail.css";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useParams, Link } from "react-router-dom";
-// import { fetchBarberById } from "../../../Store/fetchBarberByIdSlice";
-// import Loading from "../../Loading/Loading";
-// import BarberCard from "../BarberCard/BarberCard";
-// import Navbar from "../Nav/Nav";
-
-// function BarberDetails() {
-//   const { id } = useParams();
-//   const barber = useSelector((state) => state.fetchBarberById.barber);
-//   const status = useSelector((state) => state.fetchBarberById.status);
-//   const error = useSelector((state) => state.fetchBarberById.error);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchBarberById(id));
-//   }, [dispatch, id]);
-
-//   if (status === "loading") {
-//     return <Loading />;
-//   }
-
-//   if (status === "failed") {
-//     return <div>{error}</div>;
-//   }
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className="Details">{barber && <BarberCard barber={barber} />}</div>
-//     </div>
-//   );
-// }
-
-// export default BarberDetails;
-
 import { useEffect, useState } from "react";
 import "./BarberDetail.css";
 import { useSelector, useDispatch } from "react-redux";
@@ -78,8 +40,10 @@ function BarberDetails() {
       <Navbar />
       <div className="Details">
         {barber && <BarberCard barber={barber} />}
-        {/* {calendars && <p>Calendars: {JSON.stringify(calendars[0].id)}</p>} */}
-        {calendars && <BarberCalendar uuid={calendars[0].id} />}
+
+        {calendars && calendars.length > 0 && (
+          <BarberCalendar uuid={calendars[0].id} />
+        )}
       </div>
     </div>
   );
